4259a8f4314b5b6457a6364d206f9adae6399316
VastRequiem <vastrequiem@gmail.com>
2025-07-25 13:08:47 -0700

    Adjusted Shiny Odds and EVs
    Changed the shiny odds from 1/8192 to 1/512.
    Also changed the maximum number of EVs a Pokemon can have from 510 to 0.

--- include/constants/pokemon.h
+++ include/constants/pokemon.h
@@ -93,7 +93,7 @@
 #define MAX_STAT_STAGE    12
 
 // Shiny odds
-#define SHINY_ODDS 8 // Actual probability is SHINY_ODDS/65536
+#define SHINY_ODDS 128 // Actual probability is SHINY_ODDS/65536  (mychange from 8 to 128)
 
 // Ribbon IDs used by TV and Pokénav
 #define CHAMPION_RIBBON       0
@@ -213,8 +213,8 @@
 #define MAX_PER_STAT_IVS 31
 #define MAX_IV_MASK 31
 #define USE_RANDOM_IVS (MAX_PER_STAT_IVS + 1)
-#define MAX_PER_STAT_EVS ((P_EV_CAP >= GEN_6) ? 252 : 255)
-#define MAX_TOTAL_EVS 510
+#define MAX_PER_STAT_EVS ((P_EV_CAP >= GEN_6) ? 0 : 0) //mychange from 252 : 255 to 0 : 0
+#define MAX_TOTAL_EVS 0 //mychange from 510 to 0
 #define EV_ITEM_RAISE_LIMIT ((I_VITAMIN_EV_CAP >= GEN_8) ? MAX_PER_STAT_EVS : 100)
 
 // Move category defines.
