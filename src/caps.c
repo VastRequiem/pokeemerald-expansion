419eaa30aadfa2a12b648e2aec03601afc0523de
Matthew <vastrequiem@gmail.com>
2026-07-17 02:04:11 -0700

    caps, encounters, and trainers
    Changed the 2nd Gym level cap to 20, made the majority of Brawly's team not have IVs, and made some small adjustments to wild encounters. Added a few more map sections to expand on nuzlocke encounters by having more uniquely named locations.

--- src/caps.c
+++ src/caps.c
@@ -10,7 +10,7 @@ u32 GetCurrentLevelCap(void)
     static const u32 sLevelCapFlagMap[][2] =
     {
         {FLAG_BADGE01_GET, 15},
-        {FLAG_BADGE02_GET, 21},
+        {FLAG_BADGE02_GET, 20},
         {FLAG_BADGE03_GET, 32},
         {FLAG_BADGE04_GET, 40},
         {FLAG_BADGE05_GET, 50},
